9e8e58fcf083b7d07b81e02db646cb0878c0f80b
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2025-08-31 16:45:17 -0400

    Supporting Wayland and comment about Podman

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -6,11 +6,23 @@ MAP_GID=${GID:-`id -g`}
 
 mkdir -p .Garmin
 
+ENV_ARGS=(
+    -e DISPLAY=unix$DISPLAY
+    -e XDG_RUNTIME_DIR
+)
+[ -n "${WAYLAND_DISPLAY:-}" ] && ENV_ARGS+=(-e WAYLAND_DISPLAY)
+
+MOUNTS=(
+  -v "/tmp/.X11-unix:/tmp/.X11-unix"
+  -v "${XDG_RUNTIME_DIR}:${XDG_RUNTIME_DIR}"
+  -v "${PWD}/.Garmin:/home/developer/.Garmin"
+  -v "${PWD}:${PWD}"
+)
+
+# If you are using podman, you will need --userns=keep-id
 docker run -it --rm \
-    -e DISPLAY=unix$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v $PWD/.Garmin:/home/developer/.Garmin \
-    -v $PWD:$PWD \
+    "${ENV_ARGS[@]}" \
+    "${MOUNTS[@]}" \
     -w $PWD \
     -u $MAP_UID:$MAP_GID \
     --privileged \
